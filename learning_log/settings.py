da49c2323b0196a8041f6d330a04809eb143d39c
kim <kim@example.com>
2023-02-25 18:35:26 +0800

    set pyanywhere-4

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@3!7e5g52c#&c1tc@#jac_t5w*q9n1@wffov+tp02le5)d(fuu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['jiapythonany.pythonanywhere.com']
